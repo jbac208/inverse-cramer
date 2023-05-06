c74074954d524e00fcf57adff84239badcaae8f8
unknown <jbac208@aucklanduni.ac.nz>
2023-05-06 23:33:52 +1200

    printing jimcramer tweets in format time: text into console

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
+username = "jimcramer"
+
 h = {
     "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAPYXBAAAAAAACLXUNDekMxqa8h%2F40K4moUkGsoc%3DTYfbDKbT3jJPCEVnMYqilB28NHfOPqkca3qaAxGfsyKCs0wRbw"
 }
@@ -8,9 +10,9 @@ with requests.post("https://api.twitter.com/1.1/guest/activate.json", headers=h)
     guest_token = r.json()["guest_token"]
     h["x-guest-token"] = guest_token
 
-    with requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=elonmusk", headers=h) as tr:
+    with requests.get(f"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={username}", headers=h) as tr:
         data = tr.json()
 
         for tweet in data:
             # extract the properties
-            print(tweet['text'])
+            print(f"{tweet['created_at']}: {tweet['text']}")
